221e816f9919517273c80abfb3088c91610de4f4
Steven Saus <steven@stevesaus.com>
2020-09-13 17:57:09 -0400

    I think I've got it pretty much working...

--- readtags.sh
+++ readtags.sh
@@ -22,37 +22,39 @@
 # Definitions - if sourced, these should already be set
 ########################################################################
 init_vars (){
-    PDF_Text=""
-    PDF_Create_Time=""
-    PDF_FNCreate_Time=""
-    PDF_Author=""
-    PDF_Keywords="" #not an array - should be space or comma separated
-    PDF_FNTags=""
-    PDF_Subject=""
-    PDF_Title=""
-    PDF_FNTitle=""
-    PDF_File="" #actual filename, either from program calling it or from $1
-    PDF_Dir=""
-    tempdir=$(mktemp -d)
+	PDF_Text=""
+	PDF_Create_Time=""
+	PDF_FNCreate_Time=""
+	PDF_Author=""
+	PDF_Keywords="" #not an array - should be space or comma separated
+	PDF_FNTags=""
+	PDF_Subject=""
+	PDF_Title=""
+	PDF_FNTitle=""
+	PDF_File="" #actual filename, either from program calling it or from $1
+	PDF_Dir=""
+	ARGVAL=""
+	FNARGVAL=""
+	tempdir=$(mktemp -d)
 }
 
 tags_to_filename (){
 
-        #write to exif first
-        exiftool -Author="${PDF_Author}" -Keywords="${PDF_Keywords}" -Subject="${PDF_Subject}" -Title="${PDF_Title}" -CreateDate="${PDF_Create_Time}" "${PDF_File}"
-        
-        mv "${PDF_File}" "$tempdir/${PDF_File}"
-        #TITLE-YYYY-MM-DD[tag tag tag tag].pdf
-        #move original to tempdir
-        
-        #create new fn string
-        T2a=$(echo "${PDF_Title}" | detox --inline)
-        T2b=$(echo "${PDF_Create_Time}" | cut -d " " -f1 | sed s/:/-/g)
-        T2FN=$(printf "%s/%s-%s[%s].pdf" "$PDF_Dir" "$T2a" "$T2b" "$PDF_Keywords")
-        echo "$T2FN"
-        cp "$tempdir/${PDF_File}" "${T2FN}"
-        #copy old to new T2FN string
-    
+		#write to exif first
+		exiftool -Author="${PDF_Author}" -Keywords="${PDF_Keywords}" -Subject="${PDF_Subject}" -Title="${PDF_Title}" -CreateDate="${PDF_Create_Time}" "${PDF_File}"
+		
+		mv "${PDF_File}" "$tempdir/${PDF_File}"
+		#TITLE-YYYY-MM-DD[tag tag tag tag].pdf
+		#move original to tempdir
+		
+		#create new fn string
+		T2a=$(echo "${PDF_Title}" | detox --inline)
+		T2b=$(echo "${PDF_Create_Time}" | cut -d " " -f1 | sed s/:/-/g)
+		T2FN=$(printf "%s/%s-%s[%s].pdf" "$PDF_Dir" "$T2a" "$T2b" "$PDF_Keywords")
+		#echo "$T2FN"
+		cp "$tempdir/${PDF_File}" "${T2FN}"
+		#copy old to new T2FN string
+	
 }
 
 
@@ -66,29 +68,27 @@ tags_to_filename (){
 ########################################################################
 get_filename_tags (){
    
-    baseFN=$(basename ${PDF_File})
-    
-    #TODO - for Mendeley renamed stuff
-    #A Decade in Internet Time Symposium on the Dynamics of the Internet and Society, September 2011 - Boyd, Marwick, Boyd - Social Privacy.pdf
-    #JOURNAL - AUTHOR - TITLE - YEAR
-    
-    #does it have tags in the filename?
-    #TODO - Check for multiple bracket sets 
-    if [[ ${baseFN} =~ "[" ]];then
-        PDF_FNTags=$(echo ${baseFN} | cut -d "[" -f2 | cut -d "]" -f1)
-        if [[ ${PDF_FNTags} =~ "[" ]];then # ensuring there isn't a [[ tag tag ]] scenario
-            PDF_FNTags=$(echo ${PDF_FNTags} | cut -d "[" -f2 | cut -d "]" -f1)
-        fi
-    fi
-    
-    #does it have a date in the filename at the end like GScan2PDF does?
-    if [[ ${baseFN} =~ [0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9] ]];then
-        PDF_FNTitle=$(echo ${baseFN} | cut -d "[" -f1 | sed -n "s/\(^.*\)-[0-9][0-9][0-9][0-9].*$/\1/p")
-        scratch3=$(echo ${baseFN} | cut -d "[" -f1 | sed -n "s/^.*-\([0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]\).*$/\1/p")
-        PDF_FNCreate_Time=$(printf "%s 00:00:00+00:00" "$scratch3")
-    else
-        PDF_FNTitle=$(echo ${baseFN} | cut -d "[" -f1)
-    fi
+	baseFN=$(basename ${PDF_File})
+	
+	#TODO - for Mendeley renamed stuff
+	#A Decade in Internet Time Symposium on the Dynamics of the Internet and Society, September 2011 - Boyd, Marwick, Boyd - Social Privacy.pdf
+	#JOURNAL - AUTHOR - TITLE - YEAR
+	
+	if [[ ${baseFN} =~ "[" ]];then
+		PDF_FNTags=$(echo ${baseFN} | cut -d "[" -f2 | cut -d "]" -f1)
+		if [[ ${PDF_FNTags} =~ "[" ]];then # ensuring there isn't a [[ tag tag ]] scenario
+			PDF_FNTags=$(echo ${PDF_FNTags} | cut -d "[" -f2 | cut -d "]" -f1)
+		fi
+	fi
+	
+	#does it have a date in the filename at the end like GScan2PDF does?
+	if [[ ${baseFN} =~ [0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9] ]];then
+		PDF_FNTitle=$(echo ${baseFN//_/ } | cut -d "[" -f1 | sed -n "s/\(^.*\)-[0-9][0-9][0-9][0-9].*$/\1/p")
+		scratch3=$(echo ${baseFN} | cut -d "[" -f1 | sed -n "s/^.*-\([0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]\).*$/\1/p")
+		PDF_FNCreate_Time=$(printf "%s 00:00:00+00:00" "$scratch3")
+	else
+		PDF_FNTitle=$(echo ${baseFN//_/ } | cut -d "[" -f1 )
+	fi
 }
 
 ########################################################################
@@ -96,194 +96,164 @@ get_filename_tags (){
 ########################################################################
 get_metadata_tags() {
 
-    PDF_Text=$(pdftohtml -stdout -s -i -noframes ${PDF_File} | html2text)
-    if [ -z "$PDF_Text" ];then
-        PDF_Text=$(pdftohtml -stdout -s -i -hidden -noframes ${PDF_File} | html2text)
-    fi
-    
-    scratch1=$(stat -c '%Y' ${PDF_File})
-    PDF_FileMod_Time=$(date -d @"${scratch1}" '+%Y:%m:%d %H:%M:%S%:z')
-    echo "${scratch1}"
-    scratch2=$(exiftool -T -sep ' ' -Author -Keywords -Subject -Title -CreateDate ${PDF_File})
-    PDF_Author=$(echo "${scratch2}" | awk -F $'\t' '{print $1}')
-    PDF_Keywords=$(echo "${scratch2}" | awk -F $'\t' '{print $2}')
-    PDF_Subject=$(echo "${scratch2}" | awk -F $'\t' '{print $3}')
-    PDF_Title=$(echo "${scratch2}" | awk -F $'\t' '{print $4}')
-    PDF_Create_Time=$(echo "${scratch2}" | awk -F $'\t' '{print $5}')
-
-
+	PDF_Text=$(pdftohtml -stdout -s -i -noframes ${PDF_File} | html2text)
+	if [ -z "$PDF_Text" ];then
+		PDF_Text=$(pdftohtml -stdout -s -i -hidden -noframes ${PDF_File} | html2text)
+	fi
+	
+	scratch1=$(stat -c '%Y' ${PDF_File})
+	PDF_FileMod_Time=$(date -d @"${scratch1}" '+%Y:%m:%d %H:%M:%S%:z')
+	scratch2=$(exiftool -T -sep ' ' -Author -Keywords -Subject -Title -CreateDate ${PDF_File})
+	PDF_Author=$(echo "${scratch2}" | awk -F $'\t' '{print $1}')
+	PDF_Keywords=$(echo "${scratch2}" | awk -F $'\t' '{print $2}')
+	PDF_Subject=$(echo "${scratch2}" | awk -F $'\t' '{print $3}')
+	PDF_Title=$(echo "${scratch2//_/ }" | awk -F $'\t' '{print $4}')
+	PDF_Create_Time=$(echo "${scratch2}" | awk -F $'\t' '{print $5}')
 
 }
 
 eval_metadata (){
-    
-    # check our document metadata against filename metadata
-    # if some is empty, copy over
-    if [[ -z ${PDF_Create_Time} ]] && [[ ! -z ${PDF_FNCreate_Time} ]];then
-        PDF_Create_Time=${PDF_FNCreate_Time}
-    fi
-    if  [[ -z ${PDF_FNCreate_Time} ]] && [[ ! -z ${PDF_Create_Time} ]];then
-        PDF_FNCreate_Time=${PDF_Create_Time}
-    fi
-    if [[ ${PDF_FNCreate_Time} != ${PDF_Create_Time} ]]
-        ARGVAL=${PDF_Create_Time}
-        FNARGVAL=${PDF_FNCreate_Time}
-        if_conflicting_tags
-        PDF_Create_Time=${ARGVAL}
-        PDF_FNCreate_Time=${ARGVAL}
-     fi   
+	
+	# check our document metadata against filename metadata
+	# if some is empty, copy over
+	if [[ -z ${PDF_Create_Time} ]] && [[ ! -z ${PDF_FNCreate_Time} ]];then
+		PDF_Create_Time=${PDF_FNCreate_Time}
+	fi
+	if  [[ -z ${PDF_FNCreate_Time} ]] && [[ ! -z ${PDF_Create_Time} ]];then
+		PDF_FNCreate_Time=${PDF_Create_Time}
+	fi
+	if [[ ${PDF_FNCreate_Time} != ${PDF_Create_Time} ]];then
+		ARGVAL=${PDF_Create_Time}
+		FNARGVAL=${PDF_FNCreate_Time}
+		if_conflicting_tags
+		PDF_Create_Time=${ARGVAL}
+		PDF_FNCreate_Time=${ARGVAL}
+	 fi   
 
-    if [[ -z ${PDF_Keywords} ]] && [[ ! -z ${PDF_FNTags} ]];then
-        PDF_Keywords=${PDF_FNTags}
-    fi
-    if  [[ -z ${PDF_FNTags} ]] && [[ ! -z ${PDF_Keywords} ]];then
-        PDF_FNTags=${PDF_Keywords}
-    fi
-    if [[ ${PDF_FNTags} != ${PDF_Keywords} ]]
-        ARGVAL=${PDF_Keywords}
-        FNARGVAL=${PDF_FNTags}
-        if_conflicting_tags
-        PDF_Keywords=${ARGVAL}
-        PDF_FNTags=${ARGVAL}
-     fi   
+	if [[ -z ${PDF_Keywords} ]] && [[ ! -z ${PDF_FNTags} ]];then
+		PDF_Keywords=${PDF_FNTags}
+	fi
+	if  [[ -z ${PDF_FNTags} ]] && [[ ! -z ${PDF_Keywords} ]];then
+		PDF_FNTags=${PDF_Keywords}
+	fi
+	if [[ ${PDF_FNTags} != ${PDF_Keywords} ]];then
+		ARGVAL=${PDF_Keywords}
+		FNARGVAL=${PDF_FNTags}
+		if_conflicting_tags
+		PDF_Keywords=${ARGVAL}
+		PDF_FNTags=${ARGVAL}
+	 fi   
 
-    
-    if [[ -z ${PDF_Title} ]] && [[ ! -z ${PDF_FNTitle} ]];then
-        PDF_Title=${PDF_FNTitle} 
-    fi
-    if  [[ -z ${PDF_FNTitle} ]] && [[ ! -z ${PDF_Title} ]];then
-        PDF_FNTitle=${PDF_Title}
-    fi
-    if [[ ${PDF_FNTitle} != ${PDF_Title} ]]
-        ARGVAL=${PDF_Title}
-        FNARGVAL=${PDF_FNTitle}
-        if_conflicting_tags
-        PDF_Title=${ARGVAL}
-        PDF_FNTitle=${ARGVAL}
-     fi   
+	
+	if [[ -z ${PDF_Title} ]] && [[ ! -z ${PDF_FNTitle} ]];then
+		PDF_Title=${PDF_FNTitle} 
+	fi
+	if  [[ -z ${PDF_FNTitle} ]] && [[ ! -z ${PDF_Title} ]];then
+		PDF_FNTitle=${PDF_Title}
+	fi
+	if [[ ${PDF_FNTitle} != ${PDF_Title} ]];then
+		ARGVAL=${PDF_Title}
+		FNARGVAL=${PDF_FNTitle}
+		if_conflicting_tags
+		PDF_Title=${ARGVAL}
+		PDF_FNTitle=${ARGVAL}
+	 fi   
 
 }
 
 if_conflicting_tags () {
-    #abstracting ARGVAL,FNARGVAL
-    if [ "${ARGVAL}" != "${FNARGVAL}" ];then 
-        Resolution=$(yad --width=400 --center --window-icon=gtk-error \
-        --borders 3 --title="Conflicting metadata" --text="Verify conflicting PDF metadata:" \
-        --checklist --list --column=choose --column=source --column=metadata:text false "${ARGVAL}" false "${FNARGVAL}" false "manual resolution")
-        if [ ! -z "${Resolution}" ];then
-            if [[ "${Resolution}" =~ "manual resolution" ]];then
-                ResolveString=$(echo "${ARGVAL} ${FNARGVAL}")
-                Resolution2=$(yad --width=400 --center --window-icon=gtk-info \
-                --borders 3 --title="PDF Metadata" --text="Verify and edit PDF metadata:" \
-                --field="Edit This String" \
-                --button=gtk-save:0 --button=gtk-cancel:1 \
-                "${ResolveString}")
-                ARGVAL=$(echo "${Resolution2}")
-                FNARGVAL=$(echo "${Resolution2}")            
-            else
-                ARGVAL=$(echo "$Resolution" | awk -F '|' '{print $2}')
-                FNARGVAL=$(echo "$Resolution" | awk -F '|' '{print $2}')
-            fi
+
+	#abstracting ARGVAL,FNARGVAL
+	if [ "${ARGVAL}" != "${FNARGVAL}" ];then 
+		Resolution=$(yad --width=400 --height=200 --center --window-icon=gtk-error \
+		--borders 3 --title="Conflicting metadata" --text="Verify conflicting PDF metadata:" \
+		--radiolist --list --column=choose:RD --column=metadata:text false "${ARGVAL}" false "${FNARGVAL}" false "manual resolution")
+        echo "${Resolution}"
+        if [[ "${Resolution}" =~ "TRUE|" ]];then
+            echo "FUK"
         fi
+		if [ ! -z "${Resolution}" ];then
+			if [[ "${Resolution}" =~ "manual resolution" ]];then
+				ResolveString=$(echo "${ARGVAL} ${FNARGVAL}")
+				Resolution2=$(yad --form --width=400 --center --window-icon=gtk-info \
+				--borders 3 --title="PDF Metadata" --text="Verify and edit PDF metadata:" \
+				--field="Edit This String" \
+				--button=gtk-cancel:1 --button=gtk-ok:0  \
+				"${ResolveString}")
+				if [ "$?" == "0" ];then
+                    
+					ARGVAL=$(echo "${Resolution2}" | awk -F '|' '{print $1}')
+					FNARGVAL=$(echo "${Resolution2}"awk -F '|' '{print $1}')            
+				fi
+			else
+				ARGVAL=$(echo "$Resolution" | awk -F '|' '{print $2}')
+				FNARGVAL=$(echo "$Resolution" | awk -F '|' '{print $2}')
+			fi
+		fi
+	fi
 }
 
 display_metadata () {
-    
-    #Using the remote server aspect of xpdf to kill it after perusal
-    xpdf -remote skipa -geometry 300x400 -bg rgb:10/16/10 -z page "${PDF_File}" &
-    
-    #TODO  - note that the exiftag values will be what is written!!
-    
-    #https://www.thelinuxrain.com/articles/multiple-item-data-entry-with-yad
-    OutString=$(yad --width=400 --center --window-icon=gtk-info \
-    --borders 3 --title="PDF Metadata" --text="Verify and edit PDF metadata:" \
-    --form --date-format="+%Y:%m:%d %H:%M:%S%:z" --item-separator="," \
-    --field="Filename" \
-    --field="Title" \
-    --field="FNTitle" \
-    --field="Subject" \
-    --field="Author" \
-    --field="Keywords" \
-    --field="Tags" \
-    --field="Creation Time":DT \
-    --field="FN Creation Time":DT \
-    --button=gtk-save:0 --button=gtk-cancel:1 --button=gtk-quit:1\
-    "${PDF_File}" "${PDF_Title}" "${PDF_FNTitle}" "${PDF_Subject}" "${PDF_Author}" "${PDF_Keywords}" "${PDF_FNTags}" "${PDF_Create_Time}" "${PDF_FNCreate_Time}" )
-    foo=$?
-    if [[ "$foo" == "0" ]];then
-        #echo "$OutString"
-        #awk out fields
-        
-        PDF_File=$(echo "$OutString" | awk -F '|' '{print $1}') 
-        PDF_Title=$(echo "$OutString" | awk -F '|' '{print $2}') 
-        PDF_FNTitle=$(echo "$OutString" | awk -F '|' '{print $3}') 
-        PDF_Subject=$(echo "$OutString" | awk -F '|' '{print $4}') 
-        PDF_Author=$(echo "$OutString" | awk -F '|' '{print $5}') 
-        PDF_Keywords=$(echo "$OutString" | awk -F '|' '{print $6}') 
-        PDF_FNTags=$(echo "$OutString" | awk -F '|' '{print $7}') 
-        PDF_Create_Time=$(echo "$OutString" | awk -F '|' '{print $8}') 
-        PDF_FNCreate_Time=$(echo "$OutString" | awk -F '|' '{print $9}')
-        #if any matching field is blank, copy over
-        eval_metadata
-        tags_to_filename
-        #./Chapter_13_Disbursements-2020-07-08[bankruptcy].pdf|Chapter 13 Disbursements|Chapter_13_Disbursements|Bankrupcty|Steven Saus|[bankruptcy]|bankruptcy|2020:07:08 00:00:00-04:00|2020-07-08 00:00:00+00:00|
-    fi 
-    echo "$foo"
-    
-    #use that foo output to know whether to exit (1) or not (0)
-    # check if mismatches
-    # resplit out the yad output to the variables
-    xpdf -remote skipa -quit
-    
+	
+	#Using the remote server aspect of xpdf to kill it after perusal
+	xpdf -remote skipa -geometry 300x400 -bg rgb:10/16/10 -z page "${PDF_File}" &
+	
+	#TODO  - note that the exiftag values will be what is written!!
+	
+	#https://www.thelinuxrain.com/articles/multiple-item-data-entry-with-yad
+	OutString=$(yad --width=400 --center --window-icon=gtk-info \
+	--borders 3 --title="PDF Metadata" --text="Verify and edit PDF metadata:" \
+	--form --date-format="+%Y:%m:%d %H:%M:%S%:z" --item-separator="," \
+	--field="Filename" \
+	--field="Title" \
+	--field="Subject" \
+	--field="Author" \
+	--field="Tags" \
+	--field="Creation Time":DT \
+	--button=gtk-save:0 --button=gtk-cancel:1 --button=gtk-quit:1\
+	"${PDF_File}" "${PDF_Title}" "${PDF_Subject}" "${PDF_Author}" "${PDF_Keywords}" "${PDF_Create_Time}" )
+	foo=$?
+	if [[ "$foo" == "0" ]];then
+		#echo "$OutString"
+		#awk out fields
+		
+		PDF_File=$(echo "$OutString" | awk -F '|' '{print $1}') 
+		PDF_Title=$(echo "$OutString" | awk -F '|' '{print $2}') 
+		
+		PDF_Subject=$(echo "$OutString" | awk -F '|' '{print $4}') 
+		PDF_Author=$(echo "$OutString" | awk -F '|' '{print $5}') 
+		PDF_Keywords=$(echo "$OutString" | awk -F '|' '{print $6}') 
+
+		PDF_Create_Time=$(echo "$OutString" | awk -F '|' '{print $8}') 
+
+		#if any matching field is blank, copy over
+	fi 
+	
+	xpdf -remote skipa -quit
+	
 }
 
 ##############################################################################
-# Are we sourced?
-# From http://stackoverflow.com/questions/2683279/ddg#34642589
+# Main
 ##############################################################################
 
-# Try to execute a `return` statement,
-# but do it in a sub-shell and catch the results.
-# If this script isn't sourced, that will raise an error.
-$(return >/dev/null 2>&1)
-
-# What exit code did that give?
-if [ "$?" -eq "0" ];then
-    #echo "[info] Function read_vcard ready to go."
-    OUTPUT=0
-else
-    OUTPUT=1
-    # These should be set already if called from a function
-    
     init_vars
-    
-    if [ "$#" = 0 ];then
-        echo "Please call this as a function or with the filename as the first argument."
-    else
-        if [ -f "$1" ];then
-            PDF_File="$1"
-            PDF_Dir=$(dirname `readlink -f ${PDF_File}`)
-            echo "$PDF_Dir"
-        fi
-        if [ ! -f "$PDF_File" ];then
-            echo "File not found..."
-            exit 1
-        fi
-        SUCCESS=0
-        get_filename_tags
-        get_metadata_tags
-        eval_metadata
-        display_metadata
-        
-        #do stuff above
-        #change the below, obviously
-        #output=$(read_vcard)
-        if [ $SUCCESS -eq 0 ];then
-            # If it gets here, it has to be standalone
-                echo "$output"  # output some data
-        else
-            exit 99
-        fi
-    fi
-fi
+	if [ "$#" = 0 ];then
+		echo "Please call this as a function or with the filename as the first argument."
+	else
+		if [ -f "$1" ];then
+			PDF_File="$1"
+			PDF_Dir=$(dirname `readlink -f ${PDF_File}`)
+		fi
+		if [ ! -f "$PDF_File" ];then
+			echo "File not found..."
+			exit 1
+		fi
+		get_filename_tags
+		get_metadata_tags
+		eval_metadata
+		display_metadata
+		eval_metadata
+		tags_to_filename
+	fi
 
